01325543fa4e9eb8ef11d79793e7366319eaa0ca
true-lnz <lansonzgroup@yandex.ru>
2025-04-20 00:05:11 +0500

    Страница тестов

---
+++ web/static/js/test.js
@@ -0,0 +1,32 @@
+const navContainer = document.querySelector('.question-navigation');
+const questionButtons = navContainer.querySelectorAll('.question-number');
+
+let isDragging = false;
+let startX;
+let scrollStart;
+
+questionButtons.forEach(button => {
+    button.addEventListener('mousedown', (e) => {
+        isDragging = true;
+        startX = e.pageX;
+        scrollStart = navContainer.scrollLeft;
+        navContainer.classList.add('dragging');
+    });
+
+    button.addEventListener('mouseup', () => {
+        isDragging = false;
+        navContainer.classList.remove('dragging');
+    });
+
+    button.addEventListener('mouseleave', () => {
+        isDragging = false;
+        navContainer.classList.remove('dragging');
+    });
+
+    button.addEventListener('mousemove', (e) => {
+        if (!isDragging) return;
+        const x = e.pageX;
+        const walk = (x - startX) * 1.5;
+        navContainer.scrollLeft = scrollStart - walk;
+    });
+});
