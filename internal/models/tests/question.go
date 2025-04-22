54efdcee24ca80df7f3242f891fa21e039a6c495
AdWanTay <63309077+AdWanTay@users.noreply.github.com>
2025-04-22 10:47:19 +0500

    Объединил модели тестов

--- internal/models/tests/question.go
+++
@@ -1,9 +0,0 @@
-package tests
-
-type Question struct {
-	ID           uint   `gorm:"primaryKey"`
-	QuestionText string `gorm:"type:text"`
-
-	TestID uint `gorm:"not null;OnDelete:CASCADE;"`
-	Test   Test `gorm:"foreignKey:TestID;references:ID"`
-}
